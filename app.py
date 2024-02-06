bfb7e4017ac4ffeb7118c11042115d0ddb8a895b
Phornpitak Kadta <86459508+PumXPer@users.noreply.github.com>
2024-02-07 03:24:31 +0700

    set

--- app.py
+++ app.py
@@ -45,15 +45,11 @@ with tab1:
             "Fixed defect (เสียงเสีย)",
             "Reversable defect (เสียงกลับได้)"
         ])
-    import streamlit as st
 
-    age = st.slider('How old are you?', 0, 130, 25)
-    st.write("I'm ", age, 'years old')
-
-    trtbps = st.slider('Resting Blood Pressure (trtbps)', 90, 200, 120, value=200)
-    chol = st.slider('Cholesterol (chol)', 100, 600, 200, value=286)
-    thalachh = st.slider('Maximum Heart Rate Achieved (thalachh)', 60, 200, 150,value=108)
-    oldpeak = st.slider('Oldpeak (ST depression induced by exercise relative to rest)', 0.0, 6.2, 1.0, value=1.5)
+    trtbps = st.slider('Resting Blood Pressure (trtbps)', 90, 200, 200)
+    chol = st.slider('Cholesterol (chol)', 100, 600, 286)
+    thalachh = st.slider('Maximum Heart Rate Achieved (thalachh)', 60, 200, 108)
+    oldpeak = st.slider('Oldpeak (ST depression induced by exercise relative to rest)', 0.0, 6.2, 1.5)
     
     
     #st.selectbox('Exercise Induced Angina (exng) - เจ็บหน้าอกจากการออกกำลังกาย', ["No", "Yes"])
